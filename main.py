06c9cf2b2732138dc7ca25440693cca2777a6e69
Kalin Harikumar V <127252539+kalinharikumar@users.noreply.github.com>
2023-08-08 11:35:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,12 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tag import pos_tag
 import random
 
-# Download NLTK resources
-#nltk.download('punkt')
-#nltk.download('averaged_perceptron_tagger')
-
-# Read paragraph from text file
 with open('c1.txt', 'r') as file:
     paragraph = file.read()
 
-# Tokenize the paragraph into sentences
 sentences = sent_tokenize(paragraph)
-
-# Rest of the code remains the same
-# ... (including the rest of the code from the previous response)
-
-# Collect MCQs, options, and correct answers
 mcq_data = []
 
-# Identify sentence patterns, keywords, and generate MCQ options in each sentence
 for i, sentence in enumerate(sentences):
     pattern, keywords = recognize_and_extract(sentence)
     options = generate_mcq_options(keywords)
@@ -33,7 +21,6 @@ for i, sentence in enumerate(sentences):
             'correct_answers': keywords[:2]
         })
 
-# Printing collected MCQ data
 for i, mcq in enumerate(mcq_data):
     print(f"MCQ {i+1}:")
     print(f"   Sentence: '{mcq['sentence']}'")
